a60c7d2d972af9d6a11939b1f119899c84ad94ca
NatanRei <nreischmura@gmail.com>
2023-10-12 19:28:52 -0300

    refactor: removing comments

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,14 +1,5 @@
 <?php
 
-use CodeIgniter\Router\RouteCollection;
 
-/**
- * @var RouteCollection $routes
- */
-$routes->get('/', 'User::index');
-$routes->get('/user/create', 'User::create');
-$routes->post('/user/store', 'User::store');
-$routes->get('/user/edit/(:num)', 'User::edit/$1');
-$routes->get('/user/delete/(:num)', 'User::delete/$1');
 
 
---
+++ app/Views/layouts/default.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
+     <link rel="stylesheet" type="text/css" href="<?= base_url('app/Views/templates/styles.css') ?>">
+
+</head>
+<body>
+    <header>
+        <h1>CI4 Usuários</h1>
+        <?= $menu ?>
+    </header>
+
+    <main>
+        <?= $content ?>
+    </main>
+
+    <footer>
+        
+    </footer>
+</body>
+</html>
